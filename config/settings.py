9add21476a105fd974cba6c7bbc33b8175a9c9fc
dev-Lei_Nad <danieludoenangjnr33@gmail.com>
2026-07-22 04:27:26 +0100

    allowed render host

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-+sce623np4!vd^k7-t*tkq)&_*%qfszxnf!d4vt=bmzxejeeb8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["konami-gqj0.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
